c2f84c4e301a638a91b6027081a4d3d6ddcc1072
Mangie Nadia Kpaka <mangiekpaka@gmail.com>
2021-02-22 00:50:05 -0500

    updated timestamp

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ function formatDate(timestamp) {
     minutes = `0${minutes}`;
   }
 
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
   let dayIndex = date.getDay();
   let days = [
     "Sunday",
